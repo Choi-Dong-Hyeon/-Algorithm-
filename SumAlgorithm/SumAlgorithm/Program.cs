047539834b7472a5408086c121b19402fe82831c
Choi <vwwwwvww@gmail.com>
2023-06-03 00:17:58 +0900

    commit

---
+++ SumAlgorithm/SumAlgorithm/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SumAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = { 100, 75, 50, 37, 90, 95 };
+            int _sum = default;
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                if (scores[i] >= 80)
+                {
+                    _sum += scores[i];
+                }
+
+            }
+
+            Console.WriteLine($"{scores.Length}명의 점수 중 80점 이상의 총점: {_sum}");
+
+        }
+    }
+}
