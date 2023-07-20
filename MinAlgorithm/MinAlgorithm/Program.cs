b8cfc2e6f8007a34d6a0b48b9d5f0ef6a527091d
Choi <vwwwwvww@gmail.com>
2023-07-20 11:28:44 +0900

    commit

---
+++ MinAlgorithm/MinAlgorithm/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MinAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //주어진 자료들 중 가장 작은 값
+
+            int min = int.MaxValue;
+
+            int[] numbers = { -2, -5, -3, -1, -7 };
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+            }
+
+            Console.WriteLine(min);
+
+        }
+    }
+}
