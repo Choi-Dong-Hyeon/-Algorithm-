3b1024e0c0efb1f76ea11c8966685a46c38d9b4d
Choi <vwwwwvww@gmail.com>
2023-07-21 11:40:06 +0900

    commit

---
+++ RankAlgorithm/RankAlgorithm/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace RankAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = { 90, 87, 100, 95, 80 };
+            int[] rankings = Enumerable.Repeat(1, 5).ToArray();
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                rankings[i] = 1;
+                for (int j = 0; j < scores.Length; j++)
+                {
+                    if (scores[i] < scores[j])
+                    {
+                        rankings[i]++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < rankings.Length; i++)
+            {
+
+                Console.WriteLine($"{scores[i],3}점 : {rankings[i]}등");
+            }
+
+        }
+    }
+}
