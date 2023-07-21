5b03f64213d849ec5b73ae99d59c6e6103828aea
Choi <vwwwwvww@gmail.com>
2023-07-21 10:51:06 +0900

    commit

---
+++ NearAlgorithm/NearAlgorithm/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace NearAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //근삿값 가까운 값 찾기
+
+            int Abs(int number) => (number < 0) ? -number : number;
+
+            int[] numbers = { 10, 20, 30, 27, 17 };
+
+            int min = int.MaxValue;
+            int target = 25;
+            int near = default;
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int abs = Abs(numbers[i] - target);
+
+                if (abs < min)
+                {
+                    min = abs; 
+                    near = numbers[i];
+                }
+            }
+
+            Console.WriteLine($"{target}과 가까운 값{near} (차이값){min}");
+
+        }
+    }
+}
