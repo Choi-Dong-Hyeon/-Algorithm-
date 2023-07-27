eef7010440247549c06a21b24120a15dc0d91ae1
Choi <vwwwwvww@gmail.com>
2023-07-27 10:03:29 +0900

    commit

---
+++ ModeAlgorithm/ModeAlgorithm/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ModeAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //최빈값 알고리즘
+            //주어진 데이터에서 가장 많이 나온(중복된) 데이터값 찾기
+
+            int[] score = { 1, 3, 4, 3, 5 };
+            int[] indexes = new int[5 + 1];
+            int max = int.MinValue;
+            int mode = 0;
+
+            for (int i = 0; i < score.Length; i++)
+            {
+                indexes[score[i]]++;
+            }
+            for(int i=0; i < indexes.Length; i++)
+            {
+                if (indexes[i] > max)
+                {
+                    max = indexes[i];
+                    mode = i;
+                }
+            }
+
+            Console.WriteLine($"최빈값 : {mode} - {max}번 나타남");
+
+        }
+    }
+}
