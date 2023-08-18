904b6975d0e4f5139c68c99cc061116d8d90487c
Choi <vwwwwvww@gmail.com>
2023-08-18 15:49:15 +0900

    commit

---
+++ SideWinderMap/SideWinderMap/Board.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+class Board
+{
+    const char CIRCLE = '\u25cf';
+    public int[,] _tile;
+    public int _size;
+
+    public enum TileType
+    {
+        Empty,
+        Wall,
+    }
+
+    public void Init(int size)
+    {
+        if (size % 2 == 0) return;
+
+        _tile = new int[size, size];
+        _size = size;
+
+        SideWinderMap();
+    }
+
+    void SideWinderMap()
+    {
+
+        for (int y = 0; y < _size; y++)
+        {
+            for (int x = 0; x < _size; x++)
+            {
+                if (x % 2 == 0 || y % 2 == 0)
+                    _tile[y, x] = (int)TileType.Wall;
+                else
+                    _tile[y, x] = (int)TileType.Empty;
+            }
+        }
+
+
+        Random rand = new Random();
+        for (int y = 0; y < _size; y++)
+        {
+            int count = 1;
+            for (int x = 0; x < _size; x++)
+            {
+                if (x % 2 == 0 || y % 2 == 0)
+                    continue;
+
+                if (y == _size - 2 && x == _size - 2)
+                    continue;
+
+                if (y == _size - 2)
+                {
+                    _tile[y, x + 1] = (int)TileType.Empty;
+                    continue;
+                }
+
+                if (x == _size - 2)
+                {
+                    _tile[y + 1, x] = (int)TileType.Empty;
+                    continue;
+                }
+
+                if (rand.Next(0, 2) == 0)
+                {
+                    _tile[y, x + 1] = (int)TileType.Empty;
+                    count++;
+                }
+                else
+                {
+                    int randomIndex = rand.Next(0, count);
+                    _tile[y + 1, x - randomIndex * 2] = (int)TileType.Empty;
+                    count = 1;
+                }
+            }
+        }
+
+    }
+
+
+    public void Render()
+    {
+        for (int y = 0; y < _size; y++)
+        {
+            for (int x = 0; x < _size; x++)
+            {
+                Console.ForegroundColor = GetTileColor((TileType)_tile[y, x]);
+                Console.Write(CIRCLE);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    ConsoleColor GetTileColor(TileType type)
+    {
+        switch (type)
+        {
+            case TileType.Empty:
+                return ConsoleColor.Green;
+            case TileType.Wall:
+                return ConsoleColor.Red;
+            default:
+                return ConsoleColor.Green;
+        }
+    }
+}
+
--- SideWinderMap/SideWinderMap/Program.cs
+++ SideWinderMap/SideWinderMap/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SideWinderMap
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+
+
+        Board board = new Board();
+        board.Init(25);
+
+        Console.CursorVisible = false;
+
+        const int WAIT_TICK = 10000 / 30;
+        int lastTick = 0;
+
+        while (true)
         {
+
+            Console.SetCursorPosition(0, 0);
+            board.Render();
         }
+
     }
 }
+
